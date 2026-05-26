98e9d8f1563bf29acfde4ca98728c4780649de3d
gioJioh <sjgio0515@naver.com>
2026-05-26 11:33:31 +0900

    컨트롤러 수정

--- src/main/kotlin/com/example/back/HomeController.kt
+++ src/main/kotlin/com/example/back/HomeController.kt
@@ -1,8 +1,10 @@
 package com.example.back
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.s3.S3Client
 
+@RestController
 class HomeController(
     private val s3Client: S3Client
 ) {
