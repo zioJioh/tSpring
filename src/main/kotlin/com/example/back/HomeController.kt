07fb6ff370699bbeac18e2a525ca4bc70943c2c6
gioJioh <sjgio0515@naver.com>
2026-05-26 15:09:57 +0900

    340

--- src/main/kotlin/com/example/back/HomeController.kt
+++ src/main/kotlin/com/example/back/HomeController.kt
@@ -1,15 +1,19 @@
 package com.example.back
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeController(
-    private val s3Service: S3Service
+    private val s3Service: S3Service,
+
+    @Value("\${custom.secretWord}")
+    private val secretWord: String = ""
 ) {
     @GetMapping
     fun main(): String {
-        return "hi"
+        return "Hi, $secretWord"
     }
 
     @GetMapping("/buckets")
---
+++ src/test/kotlin/com/example/back/S3Service.kt
@@ -0,0 +1,7 @@
+package com.example.back
+
+class S3ServiceImpl : S3Service {
+    override fun getBucketNames(): List<String> {
+        return listOf("")
+    }
+}
